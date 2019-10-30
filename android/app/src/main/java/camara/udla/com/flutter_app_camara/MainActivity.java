ebc5a90a6bc0cc8cab11831a74e2ab6c3b71dbfd
egudino <vale>
2019-10-30 17:30:07 -0500

    initial Commit

---
+++ android/app/src/main/java/camara/udla/com/flutter_app_camara/MainActivity.java
@@ -0,0 +1,13 @@
+package camara.udla.com.flutter_app_camara;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
